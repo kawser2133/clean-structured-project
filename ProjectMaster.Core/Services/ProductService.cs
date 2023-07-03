baf72353b6ac352faaae7d0dd554282bea643435
Kawser Hamid <kawser2133@gmail.com>
2023-07-03 18:21:05 +0600

    Product service update

--- ProjectMaster.Core/Services/ProductService.cs
+++ ProjectMaster.Core/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using ProjectMaster.Core.Entities.General;
+using ProjectMaster.Core.Interfaces.IMapper;
 using ProjectMaster.Core.Interfaces.IRepositories;
 using ProjectMaster.Core.Interfaces.IServices;
 using System;
@@ -11,18 +12,25 @@ namespace ProjectMaster.Core.Services
 {
     public class ProductService : IProductService
     {
+        private readonly IBaseMapper<Product, ProductViewModel> _productViewModelMapper;
+        private readonly IBaseMapper<ProductViewModel, Product> _productMapper;
         private readonly IProductRepository _productRepository;
 
-        public ProductService(IProductRepository productRepository)
+        public ProductService(
+            IBaseMapper<Product, ProductViewModel> productViewModelMapper,
+            IBaseMapper<ProductViewModel, Product> productMapper,
+            IProductRepository productRepository)
         {
+            _productMapper = productMapper;
+            _productViewModelMapper = productViewModelMapper;
             _productRepository = productRepository;
         }
 
+
         public async Task<ProductViewModel> Create(ProductViewModel model)
         {
-
-
-            return await _productRepository.Create(model);
+            var data = _productMapper.MapModel(model);
+            return _productViewModelMapper.MapModel(await _productRepository.Create(data));
         }
 
         public Task Delete(int id)
