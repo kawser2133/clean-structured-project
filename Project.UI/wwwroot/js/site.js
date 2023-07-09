175546705994e7f36a8a0e5fd223f70735b20acb
kawser2133 <kawser2133@gmail.com>
2023-07-09 23:18:59 +0600

    Layout Update

--- Project.UI/wwwroot/js/site.js
+++ Project.UI/wwwroot/js/site.js
@@ -1,21 +1,10 @@
 ﻿$(document).ready(function () {
-            $("#sidebar").mCustomScrollbar({
-                theme: "minimal"
-            });
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar, #content').toggleClass('active');
-                $('.collapse.in').toggleClass('in');
-                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-            });
-});
-
-
-$(document).ready(function () {
-    $('nav ul li a').click(function (e) {
-        $('nav li.active').removeClass('active');
-
-        var $parent = $(this).parent();
-        $parent.addClass('active');
-        e.preventDefault();
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
     });
 });
